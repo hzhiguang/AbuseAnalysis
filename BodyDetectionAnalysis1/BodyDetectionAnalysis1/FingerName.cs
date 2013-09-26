b146f46b3be518fc55e2cd82ed0bf68ba2774757
hzhiguang <rune199@hotmail.com>
2013-09-26 17:55:35 +0800

    2nd Commit

---
+++ BodyDetectionAnalysis1/BodyDetectionAnalysis1/FingerName.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BodyDetectionAnalysis1
+{
+    public class FingerNameClass
+    {
+        public enum FingerName { LITTLE, RING, MIDDLE, INDEX, THUMB, UNKNOWN };
+
+        public FingerName getNext(Enum current)
+        {
+            //set current index to 0
+            int currentIdx = 0;
+            //initialize a Enum FingerName
+            FingerName f = FingerName.UNKNOWN;
+            //loop to find the current Enum index and value
+            foreach (var value in Enum.GetValues(typeof(FingerName)))
+            {
+                if (current == value)
+                {
+                    f = (FingerName)value;
+                    break;
+                }
+                else
+                {
+                    currentIdx++;
+                }
+            }
+            //add 1 to get next index of Enum
+            int nextIdx = currentIdx + 1;
+            //check if next index value is UNKNOWN
+            if (nextIdx == Enum.GetValues(typeof(FingerName)).Length)
+            {
+                nextIdx = 0;
+            }
+            //return the value of next Enum
+            return f;
+        } // end of getNext()
+
+        public FingerName getPrev(Enum current)
+        {
+            //set current index to last
+            int currentIdx = 5;
+            //initialize a Enum FingerName 
+            FingerName f = FingerName.UNKNOWN;
+            //loop to find the current Enum index and value
+            foreach (var value in Enum.GetValues(typeof(FingerName)))
+            {
+                if (current == value)
+                {
+                    f = (FingerName)value;
+                    break;
+                }
+                else
+                {
+                    currentIdx--;
+                }
+            }
+            //minus 1 to get previous index of Enum
+            int prevIdx = currentIdx - 1;
+            //check if previous index is more than 0
+            if (prevIdx < 0)
+            {
+                prevIdx = (FingerName.GetValues(typeof(FingerName)).Length) - 1;
+            }
+            //return the value of previous Enum
+            return f;
+        } // end of getPrev()
+    }
+}
+
--- BodyDetectionAnalysis1/BodyDetectionAnalysis1/Form1.cs
+++ BodyDetectionAnalysis1/BodyDetectionAnalysis1/Form1.cs
@@ -15,10 +15,10 @@ using AForge.Imaging;
 using AForge.Imaging.Filters;
 using Emgu.CV;
 using Emgu.CV.GPU;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.VideoSurveillance;
 using Emgu.Util;
-using Emgu.CV.CvEnum;
 
 namespace BodyDetectionAnalysis1
 {
@@ -45,8 +45,21 @@ namespace BodyDetectionAnalysis1
         private Point cogPt; // center of gravity (COG) of contour
         private int contourAxisAngle; // contour's main axis angle relative to the horiz (in degrees)
         private List<Point> fingerTips;
+        private List<FingerNameClass.FingerName> namedFingers;
         private static float SMALLEST_AREA = 600.0f; // ignore smaller contour areas
-        private MemStorage contourStorage;
+        private MemStorage contourStorage, approxStorage, hullStorage, defectsStorage;
+        private static int MAX_POINTS = 20; // max number of points stored in an array
+        // defects data for the hand contour
+        private Point[] tipPts, foldPts;
+        private float[] depths;
+        private Font msgFont;
+
+        private static int MIN_FINGER_DEPTH = 20;
+        private static int MAX_FINGER_ANGLE = 60; // degrees
+        private static int MIN_THUMB = 120; // angle ranges
+        private static int MAX_THUMB = 200;
+        private static int MIN_INDEX = 60;
+        private static int MAX_INDEX = 120;
 
         public Form1()
         {
@@ -67,7 +80,7 @@ namespace BodyDetectionAnalysis1
             {
                 if (currentFrame != null)
                 {
-                    CascadeClassifier fullBodyCascade = new CascadeClassifier(fb);
+                    /*CascadeClassifier fullBodyCascade = new CascadeClassifier(fb);
                     Image<Gray, Byte> grayFrame = currentFrame.Convert<Gray, Byte>();
                     Rectangle[] fullBodyDetected = fullBodyCascade.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty, Size.Empty);
                     foreach (Rectangle fullBody in fullBodyDetected)
@@ -76,8 +89,12 @@ namespace BodyDetectionAnalysis1
                         imgs.Add(currentFrame.Clone());
                     }
                     capturedImageBox.Image = currentFrame.Clone();
-                    imageBox1.Image = grayFrame.Clone();
+                    imageBox1.Image = grayFrame.Clone();*/
 
+                    capturedImageBox.Image = currentFrame.Clone();
+                    MIplImage img = currentFrame.MIplImage;
+                    update(img);
+                    
                 }
             }
         }
@@ -89,28 +106,22 @@ namespace BodyDetectionAnalysis1
 
         private void update(MIplImage im)
         {
-            IntPtr image = im.roi;
-            IntPtr scaleImage = scaleImg.roi;
-            IntPtr hsvImage = hsvImg.roi;
-            IntPtr imageThreshed = imgThreshed.roi;
-
-            CvInvoke.cvResize(image, scaleImage, INTER.CV_INTER_CUBIC); // reduce the size of the image to make processing faster
+            CvInvoke.cvResize(im.roi, scaleImg.roi, INTER.CV_INTER_CUBIC); // reduce the size of the image to make processing faster
 
             // convert image format to HSV
-            CvInvoke.cvCvtColor(scaleImage, hsvImage, COLOR_CONVERSION.CV_BGR2HSV);
+            CvInvoke.cvCvtColor(scaleImg.roi, hsvImg.roi, COLOR_CONVERSION.CV_BGR2HSV);
 
             // threshold image using loaded HSV settings for user's glove
-            CvInvoke.cvInRangeS(hsvImage, new MCvScalar(hueLower, satLower, briLower, 0),
+            CvInvoke.cvInRangeS(hsvImg.roi, new MCvScalar(hueLower, satLower, briLower, 0),
                                         new MCvScalar(hueUpper, satUpper, briUpper, 0),
-                                        imageThreshed);
+                                        imgThreshed.roi);
 
-            CvInvoke.cvMorphologyEx(imageThreshed, imageThreshed, imageThreshed, imageThreshed, CV_MORPH_OP.CV_MOP_OPEN, 1);
+            CvInvoke.cvMorphologyEx(imgThreshed.roi, imgThreshed.roi, imgThreshed.roi, imgThreshed.roi, CV_MORPH_OP.CV_MOP_OPEN, 1);
 
-            // erosion followed by dilation on the image to remove 
+            // erosion followed by dilation on the image to remove
             // specks of white while retaining the image size
             MCvSeq bigContour = findBiggestContour(imgThreshed);
-            IntPtr bigC = bigContour.ptr;
-            if (bigC == null)
+            if (bigContour.ptr == null)
             {
                 return;
             }
@@ -121,21 +132,19 @@ namespace BodyDetectionAnalysis1
 
         private MCvSeq findBiggestContour(MIplImage imgThreshed)
         {
-            IntPtr imageThreshed = imgThreshed.roi;
             MCvSeq bigContour = new MCvSeq(); // generate all the contours in the threshold image as a list
             MCvSeq contours = new MCvSeq();
-            IntPtr con = contours.ptr;
-            CvInvoke.cvFindContours(imageThreshed, contourStorage, ref con, System.Runtime.InteropServices.Marshal.SizeOf(typeof(MCvContour)),
+            CvInvoke.cvFindContours(imgThreshed.roi, contourStorage, ref contours.ptr, System.Runtime.InteropServices.Marshal.SizeOf(typeof(MCvContour)),
                 RETR_TYPE.CV_RETR_LIST,
                 CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE,
                 new Point(0, 0));
             // find the largest contour in the list based on bounded box size
             float maxArea = SMALLEST_AREA;
-            while (con != null)
+            while (contours.ptr != null)
             {
                 if (contours.elem_size > 0)
                 {
-                    MCvBox2D box = CvInvoke.cvMinAreaRect2(con, contourStorage);
+                    MCvBox2D box = CvInvoke.cvMinAreaRect2(contours.ptr, contourStorage);
                     SizeF size = box.size;
                     float area = size.ToPointF().X * size.ToPointF().Y;
                     if (area > maxArea)
@@ -144,7 +153,7 @@ namespace BodyDetectionAnalysis1
                         bigContour = contours;
                     }
                 }
-                con = contours.h_next;
+                contours.ptr = contours.h_next;
             }
             return bigContour;
         }
@@ -152,8 +161,7 @@ namespace BodyDetectionAnalysis1
         private void extractContourInfo(MCvSeq bigContour, int scale)
         {
             MCvMoments moments = new MCvMoments();
-            IntPtr bigC = bigContour.ptr;
-            CvInvoke.cvMoments(bigC, ref moments, 1);
+            CvInvoke.cvMoments(bigContour.ptr, ref moments, 1);
 
             // center of gravity
             double m00 = CvInvoke.cvGetSpatialMoment(ref moments, 0, 0);
@@ -187,6 +195,11 @@ namespace BodyDetectionAnalysis1
             }
         }
 
+        private double toDegree(double angle)
+        {
+            return angle * (180.0 / Math.PI);
+        }
+
         private int calculateTilt(double m11, double m20, double m02)
         {
             double diff = m20 - m02;
@@ -201,7 +214,7 @@ namespace BodyDetectionAnalysis1
             }
 
             double theta = 0.5 * Math.Atan2(2 * m11, diff);
-            int tilt = (int)Math.Round(Math.toDegrees(theta));
+            int tilt = (int) Math.Round(toDegree(theta));
             if ((diff > 0) && (m11 == 0))
                 return 0;
             else if ((diff < 0) && (m11 == 0))
@@ -215,6 +228,226 @@ namespace BodyDetectionAnalysis1
             else if ((diff < 0) && (m11 < 0)) // -90 to -45
                 return (180 + tilt); // change to counter-clockwise angle
             return 0;
+        }
+
+        private void findFingerTips(MCvSeq bigContour, int scale)
+        {
+            MCvSeq approxContour = new MCvSeq();
+            MCvSeq hullSeq = new MCvSeq();
+            MCvSeq defects = new MCvSeq();
+            
+            approxContour.ptr = CvInvoke.cvApproxPoly(bigContour.ptr, System.Runtime.InteropServices.Marshal.SizeOf(typeof(MCvContour)), approxStorage, APPROX_POLY_TYPE.CV_POLY_APPROX_DP, 3, 1); // reduce number of points in the contour
+            hullSeq.ptr = CvInvoke.cvConvexHull2(approxContour.ptr, hullStorage, ORIENTATION.CV_COUNTER_CLOCKWISE, 0); // find the convex hull around the contour
+            defects.ptr = CvInvoke.cvConvexityDefects(approxContour.ptr, hullSeq.ptr, defectsStorage); // find the defect differences between the contour and hull
+            
+            int defectsTotal = defects.total;
+            if (defectsTotal > MAX_POINTS) {
+                defectsTotal = MAX_POINTS;
+            }
+
+            // copy defect information from defects sequence into arrays
+            for (int i = 0; i < defectsTotal; i++)
+            {
+                IntPtr pntr = CvInvoke.cvGetSeqElem(defects.ptr, i);
+                MCvConvexityDefect cdf = new MCvConvexityDefect();
+                cdf.StartPointPointer = pntr;
+
+                Point startPt = cdf.StartPoint;
+                double sx = startPt.X;
+                double sy = startPt.Y;
+                tipPts[i] = new Point((int)Math.Round(sx * scale), (int)Math.Round(sy * scale)); // array contains coords of the fingertips
+
+                Point endPt = cdf.EndPoint;
+                Point depthPt = cdf.DepthPoint;
+                double dx = depthPt.X;
+                double dy = depthPt.Y;
+                foldPts[i] = new Point((int)Math.Round(dx * scale), (int)Math.Round(dy * scale)); //array contains coords of the skin fold between fingers
+                depths[i] = cdf.Depth * scale; // array contains distances from tips to folds
+
+                //reduceTips(defectsTotal, tipPts, foldPts, depths);
+            }
+        }
+
+        private void reduceTips(int numPoints, Point[] tipPts, Point[] foldPts, float[] depths)
+        {
+            fingerTips.Clear();
+            for (int i = 0; i < numPoints; i++)
+            {
+                if (depths[i] < MIN_FINGER_DEPTH)
+                {
+                    continue;
+                }
+                // look at fold points on either side of a tip
+                int pdx = (i == 0) ? (numPoints - 1) : (i - 1); // predecessor of i
+                int sdx = (i == numPoints - 1) ? 0 : (i + 1); // successor of i
+                int angle = angleBetween(tipPts[i], foldPts[pdx], foldPts[sdx]);
+                if (angle >= MAX_FINGER_ANGLE)
+                {
+                    continue; // angle between finger and folds too wide
+                }
+                // this point is probably a fingertip, so add to list
+                fingerTips.Add(tipPts[i]);
+            }
+        }
+
+        private int angleBetween(Point tip, Point next, Point prev) // calculate the angle between the tip and its neighboring folds (in integer degrees)
+        {
+            return Math.Abs((int)Math.Round(toDegree(Math.Atan2(next.X - tip.X, next.Y - tip.Y) - Math.Atan2(prev.X - tip.X, prev.Y - tip.Y))));
+        }
+
+        private void nameFingers(Point cogPt, int contourAxisAngle, List<Point> fingerTips)
+        {
+            namedFingers.Clear();
+            for (int i = 0; i < fingerTips.Count; i++)
+            {
+                namedFingers.Add(FingerNameClass.FingerName.UNKNOWN);
+            }
+
+            labelThumbIndex(fingerTips, namedFingers);
+            labelUnknowns(namedFingers);
+        }
+
+        private void labelThumbIndex(List<Point> fingerTips, List<FingerNameClass.FingerName> nms)
+        {
+            bool foundThumb = false;
+            bool foundIndex = false;
+            int i = fingerTips.Count - 1;
+            while ((i >= 0))
+            {
+                int angle = angleToCOG(fingerTips.ElementAt(i), cogPt, contourAxisAngle);
+                //check for thumb
+                if ((angle <= MAX_THUMB) && (angle > MIN_THUMB) && !foundThumb)
+                {
+                    nms.RemoveAt(i);
+                    nms.Insert(i, FingerNameClass.FingerName.THUMB);
+                    foundThumb = true;
+                }
+
+                // check for index
+                if ((angle <= MAX_INDEX) && (angle > MIN_INDEX) && !foundIndex)
+                {
+                    nms.RemoveAt(i);
+                    nms.Insert(i, FingerNameClass.FingerName.INDEX);
+                    foundIndex = true;
+                }
+                i--;
+            }
+        }
+
+        private int angleToCOG(Point tipPt, Point cogPt, int contourAxisAngle)
+        {
+            int yOffset = cogPt.Y - tipPt.Y; // make y positive up screen
+            int xOffset = tipPt.X - cogPt.X;
+            double theta = Math.Atan2(yOffset, xOffset);
+            int angleTip = (int)Math.Round(toDegree(theta));
+            return angleTip + (90 - contourAxisAngle);
+            // this ensures that the hand is orientated straight up
+        }
+
+        private void labelUnknowns(List<FingerNameClass.FingerName> nms)
+        {
+            // find first named finger
+            int i = 0;
+            while ((i < nms.Count) && (nms.ElementAt(i) == FingerNameClass.FingerName.UNKNOWN))
+            {
+                i++;
+                if (i == nms.Count)
+                { // no named fingers found, so give up
+                    return;
+                }
+                FingerNameClass.FingerName name = nms.ElementAt(i);
+                labelPrev(nms, i, name); // fill-in backwards
+                labelFwd(nms, i, name); // fill-in forwards
+            }
+        }
+
+        private void labelPrev(List<FingerNameClass.FingerName> nms, int i, FingerNameClass.FingerName name)// move backwards through fingers list labeling unknown fingers
+        {
+            FingerNameClass fnc = new FingerNameClass();
+            i--;
+            while ((i >= 0) && (name != FingerNameClass.FingerName.UNKNOWN))
+            {
+                if (nms.ElementAt(i) == FingerNameClass.FingerName.UNKNOWN)
+                { // unknown finger
+                    name = fnc.getPrev(name);
+                    if (!usedName(nms, name))
+                    {
+                        nms.RemoveAt(i);
+                        nms.Insert(i, name);
+                    }
+                }
+                else
+                { // finger is named already
+                    name = nms.ElementAt(i);
+                }
+                i--;
+            }
+        }
+
+        private void labelFwd(List<FingerNameClass.FingerName> nms, int i, FingerNameClass.FingerName name) // move forward through fingers list labelling unknown fingers
+        {
+            FingerNameClass fnc = new FingerNameClass();
+            i++;
+            while ((i < nms.Count) && (name != FingerNameClass.FingerName.UNKNOWN))
+            {
+                if (nms.ElementAt(i) == FingerNameClass.FingerName.UNKNOWN)
+                {  // unknown finger
+                    name = fnc.getNext(name);
+                    if (!usedName(nms, name))
+                    {
+                        nms.RemoveAt(i);
+                        nms.Insert(i, name);
+                    }
+                }
+                else
+                {    // finger is named already
+                    name = nms.ElementAt(i);
+                }
+                i++;
+            }
+        }
+
+        private bool usedName(List<FingerNameClass.FingerName> nms, FingerNameClass.FingerName name) // does the fingers list contain name already?
+        {
+            foreach (FingerNameClass.FingerName fn in nms)
+                if (fn == name)
+                {
+                    return true;
+                }
+            return false;
+        }
+
+        public void draw(Graphics g)
+        {
+            if (fingerTips.Count == 0)
+            {
+                return;
+            }
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            SolidBrush yellow = new SolidBrush(Color.Yellow);
+            SolidBrush red = new SolidBrush(Color.Red);
+            SolidBrush green = new SolidBrush(Color.Green);
+            Pen penYellow = new Pen(yellow, 4);
+            Pen penRed = new Pen(red, 4);
+            Pen penGreen = new Pen(green, 4);
+            // label tips in red or green, and draw lines to named tips
+            msgFont = new Font("SansSerif", 18, FontStyle.Bold, GraphicsUnit.Pixel);
+            for (int i = 0; i < fingerTips.Count; i++)
+            {
+                Point pt = fingerTips.ElementAt(i);
+                if (namedFingers.ElementAt(i) == FingerNameClass.FingerName.UNKNOWN)
+                {
+                    g.DrawEllipse(penRed, cogPt.X, cogPt.Y, pt.X, pt.Y);
+                    g.DrawString("" + i, msgFont, red, pt.X, pt.Y - 10);
+                }
+                else // draw yellow line to the named fingertip from COG
+                {
+                    g.DrawLine(penYellow, cogPt, pt);
+                    g.DrawEllipse(penGreen, pt.X-8, pt.Y-8, 16, 16);
+                    g.DrawString(namedFingers.ElementAt(i).ToString().ToLower(), msgFont, green, pt.X, pt.Y - 10);
+                }
+            }
+            g.FillEllipse(green, cogPt.X-8, cogPt.Y-8, 16, 16);
         }
     }
 }
