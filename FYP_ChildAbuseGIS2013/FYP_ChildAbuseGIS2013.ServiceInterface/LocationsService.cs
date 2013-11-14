54305f0dd5ffa8907f3e76c5579954c229ffa1c4
hzhiguang <rune199@hotmail.com>
2013-11-14 17:49:29 +0800

    ascasc

--- FYP_ChildAbuseGIS2013/FYP_ChildAbuseGIS2013.ServiceInterface/LocationsService.cs
+++ FYP_ChildAbuseGIS2013/FYP_ChildAbuseGIS2013.ServiceInterface/LocationsService.cs
@@ -11,11 +11,13 @@ namespace FYP_ChildAbuseGIS2013.ServiceInterface
 {
     public class LocationsService : Service
     {
+        //Get all location data
         public LocationResult Get(Locations request)
         {
             return new LocationResult { Location = Db.Select<Location>() };
         }
 
+        //Insert 1 location object
         public CreateLocationResult Post(CreateLocation request)
         {
             Db.ExecuteSql("INSERT INTO location (address, x, y, geom) Values ('" + request.address + "','" + request.x + "','" + request.y + "', ST_GeomFromText('POINT(" + request.x + " " + request.y + ")', 3414))");
@@ -29,6 +31,7 @@ namespace FYP_ChildAbuseGIS2013.ServiceInterface
             return new CreateLocationResult { Location = location };
         }
 
+        //Get 1 location object by id
         public LocationDetailsResult Get(GetLocationDetails request)
         {
             var location = Db.GetByIdOrDefault<Location>(request.ID);
---
+++ FYP_ChildAbuseGIS2013/FYP_ChildAbuseGIS2013/video.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Design.Master" AutoEventWireup="true" CodeBehind="video.aspx.cs" Inherits="FYP_ChildAbuseGIS2013.video" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
